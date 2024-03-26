39695db63546a4657b5a2e95d83fb0c0c512052b
Jonathan Shuman <jonathan@shuman.org>
2024-03-26 09:09:28 -0700

    working game of black jack!

--- Python/Needs_Work/Black Jack/main.py
+++ Python/Needs_Work/Black Jack/main.py
@@ -1,5 +1,6 @@
 import random
 import os
+import time
 
 os.system('cls')
 
@@ -25,9 +26,9 @@ class card:
 class hand:
     def __init__(self, card_array):
         self.card_array = card_array
-        self.value = self.return_hand_value()
+        self.update_hand_value()
 
-    def return_hand_value(self):
+    def update_hand_value(self):
         sum = 0
         num_aces = 0
         for card in self.card_array:
@@ -37,9 +38,7 @@ class hand:
         if (sum > 21 and num_aces >= 1):
             for aces in range(num_aces):
                 sum -= 10
-                if (sum <= 21):
-                    return sum
-        return sum
+        self.value = sum
 
 
 def deal(hand, deck):
@@ -48,13 +47,11 @@ def deal(hand, deck):
         hand.append(deck[last_card])
         deck.pop()
 
-def print_hand_basic(hand_name, hand):
-    print(hand_name + "\n###############")
-    for card in hand:
-        print(card.return_card())
-    print()
-
 def print_hand_fancy(hand, is_dealer):
+    card_string1 = ""
+    card_string2 = ""
+    card_string3 = ""
+    card_string4 = ""
     count = 0
     for card in hand:
         match card.suit:
@@ -67,16 +64,17 @@ def print_hand_fancy(hand, is_dealer):
             case "club":
                 first_letter = "♣"
         if is_dealer and count == 0:
-            print("-----")
-            print("|   |")
-            print("|   |")
-            print("-----")
+            card_string1 += ("-----")
+            card_string2 += ("|   |")
+            card_string3 += ("|   |")
+            card_string4 += ("-----")
         else:
-            print("-----")
-            print("|" + first_letter[0].upper() + "  |")
-            print("|  " + card.value + "|")
-            print("-----")
+            card_string1 += ("-----")
+            card_string2 += ("|" + first_letter[0].upper() + "  |")
+            card_string3 += ("|  " + card.value + "|")
+            card_string4 += ("-----")
         count += 1
+    print(card_string1 + "\n" + card_string2 + "\n" + card_string3 + "\n" + card_string4)
 
 def create_deck(deck):
     suit_list = ["diamond", "heart", "spade", "club"]
@@ -92,7 +90,51 @@ def start_game(deck, p_hand, d_hand):
     deal(d_hand, deck)
     return (deck, hand(p_hand), hand(d_hand))
 
+def print_all_fancy(d_hand, p_hand, d_up):
+    os.system("cls")
+    print_hand_fancy(d_hand.card_array, d_up)
+    print("\n\n")
+    print_hand_fancy(p_hand.card_array, False)
+
 card_deck, player_hand, dealer_hand = start_game([], [], [])
-print_hand_fancy(dealer_hand.card_array, True)
-print("\n\n")
-print_hand_fancy(player_hand.card_array, False)
+print_all_fancy(dealer_hand, player_hand, True)
+
+running = True
+while running:
+    game_over = False
+    user_input = input("Hit (H), or Stay (S)? ")
+    if user_input.lower() == "h":
+        os.system("cls")
+        player_hand.card_array.append(card_deck.pop())
+        print_all_fancy(dealer_hand, player_hand, True)
+        player_hand.update_hand_value()
+        if player_hand.value > 21:
+            game_over = True
+    elif user_input.lower() == "s":
+        os.system("cls")
+        game_over = True
+        print_all_fancy(dealer_hand, player_hand, False)
+        while dealer_hand.value < 17:
+            time.sleep(1)
+            dealer_hand.card_array.append(card_deck.pop())
+            print_all_fancy(dealer_hand, player_hand, False)
+            dealer_hand.update_hand_value()
+    elif user_input.lower() == "exit":
+        running = False
+    if game_over:
+        dealer_hand.update_hand_value()
+        player_hand.update_hand_value()
+        if player_hand.value > 21:
+            print("You lose! Do you want to play again?")
+        elif dealer_hand.value > 21 or dealer_hand.value < player_hand.value:
+            print("You win! Do you want to play again?")
+        elif dealer_hand.value == player_hand.value:
+            print("It's a tie! Do you want to play again?")
+        play_again = input("Y/N> ")
+        if play_again.lower() == "y":
+            card_deck, player_hand, dealer_hand = start_game([], [], [])
+            print_all_fancy(dealer_hand, player_hand, True)
+        elif play_again.lower() == "n":
+            running = False
+
+os.system('cls')
