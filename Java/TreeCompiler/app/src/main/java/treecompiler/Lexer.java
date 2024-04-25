103721863d5341e4842513397ab5795fe12cc0f3
TreeWonTwoToFor <jonathan-git@shuman.org>
2024-04-24 17:06:11 -0700

    started a compiler in Java for a prog language

---
+++ Java/TreeCompiler/app/src/main/java/treecompiler/Lexer.java
@@ -0,0 +1,13 @@
+package treecompiler;
+
+public class Lexer {
+    private int value = 0;
+
+    public Lexer() {
+        this.value = 10;
+    }
+
+    public int returnValue() {
+        return this.value;
+    }
+}
