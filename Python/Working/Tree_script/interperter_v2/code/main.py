976b9016c6714664e3c8464abe2d4ba9d05b8004
Jonathan Shuman <jonathan@shuman.org>
2024-04-26 13:57:13 -0700

    small cleaning

--- Python/Working/Tree_script/interperter_v2/code/main.py
+++ Python/Working/Tree_script/interperter_v2/code/main.py
@@ -11,24 +11,15 @@ class variable():
 def parse_text(user_input):
     return parser.return_tokens(parser.format_text(user_input))
 
-input_method = "file"
 current_file = "file.trs"
 
 def run_trs():
     os.system('cls')
-    running = True
-    while running:
-        if input_method == "console":
-            user_input = input("Tree Script>")
-            print(parse_text(user_input))
-            if user_input == "exit": running = False
-        if input_method == "file":
-            print("Tree Script")
-            file = open(current_file)
-            user_input = file.read()
-            parsed_tokens = parse_text(user_input)
-            interperter.interpert(parsed_tokens)
-            file.close()
-            running = False
+    print("Tree Script")
+    file = open(current_file)
+    user_input = file.read()
+    parsed_tokens = parse_text(user_input)
+    interperter.interpert(parsed_tokens)
+    file.close()
 
 run_trs()
