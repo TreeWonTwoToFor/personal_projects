a6178927fed16b613582360c04432fd7dd1484e1
TreeWonTwoToFor <jonathan-git@shuman.org>
2024-05-02 22:06:33 -0700

    made a 2d array for zones, and also added basic movement. Objects next!

--- Python/Needs_Work/Zork clone/main.py
+++ Python/Needs_Work/Zork clone/main.py
@@ -4,7 +4,28 @@ import os
 
 os.system('cls')
 
-firstZone = Zone.zone(0, 0, [], "Test Zone")
-firstZone.print_zone()
-print()
-firstZone.print_desc()
+player_x = 0
+player_y = 0
+map_size = (5,5)
+
+Zone.make_map(map_size)
+
+running = True
+while running:
+    os.system('cls')
+    Zone.give_scene(Zone.get_zone(player_x, player_y))
+    player_direction = input("Where do you want to move? ")
+    if player_direction == "north" and player_y > 0:
+        player_y -= 1
+    elif player_direction == "south" and player_y < map_size[1]-1:
+        player_y += 1
+    elif player_direction == "west" and player_x > 0:
+        player_x -= 1
+    elif player_direction == "east" and player_x < map_size[0]-1:
+        player_x += 1
+    elif player_direction == "exit":
+        os.system('cls')
+        running = False
+        break
+    else:
+        print("inavlid input")
--- Python/Needs_Work/Zork clone/pack/Zone.py
+++ Python/Needs_Work/Zork clone/pack/Zone.py
@@ -12,4 +12,19 @@ class zone:
         print(f"{line_len}\n(x, y): ({self.x, self.y})\nobject_list = {self.object_list}\ndescription: {self.description}\n{line_len}")
 
     def print_desc(self):
-        print(self.description)
+        print(self.description)  
+
+zone_array = []
+
+def get_zone(x,y):
+    return zone_array[y][x]
+
+def give_scene(zone):
+    zone.print_desc()
+
+def make_map(map_size):
+    for i in range(map_size[0]):
+        zone_row = []
+        for j in range(map_size[1]):
+            zone_row.append(zone(j, i, [], f"You are currently at {j, i}"))
+        zone_array.append(zone_row)
