e79d22beecf67b7dbbbf4736027b8c8ef3683912
TreeWonTwoToFor <jonthan-github@shuman.org>
2024-07-16 20:38:54 -0700

    fixed multiple 3d perspective bugs, it runs great!

--- Python/Needs_Work/TreeOS/World/world_package/3d_renderer.py
+++ Python/Needs_Work/TreeOS/World/world_package/3d_renderer.py
@@ -2,13 +2,14 @@ import pygame
 import time
 import os
 import json
+import math
 
 pygame.init()
 
 screenX = 1080
 screenY = 720
 bg_color = (34, 238, 85)
-FPS = 30
+FPS = 60
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((screenX, screenY), pygame.RESIZABLE)
@@ -24,15 +25,17 @@ class camera:
         print(f"{self.x} {self.y} {self.z} \n{self.FL}")
 
     def get_projected(self, point):
-        if (self.FL + point.z + self.z) == 0:
+        can_draw = True
+        # projected = (Focal Length * axis)/(Focal Length + depth)
+        if (self.FL + point.z + self.z) < 0:
+            can_draw = False
+        if (self.FL + point.z + self.z) <= 0:
             x_projected = (self.FL * (point.x + self.x))
+            y_projected = (self.FL * (point.y - self.y))
         else:
             x_projected = (self.FL * (point.x + self.x))/(self.FL + (point.z + self.z))
-        if (self.FL + point.z + self.z) == 0:
-            y_projected = (self.FL * (point.y + self.y))
-        else:
-            y_projected = (self.FL * (point.y + self.y))/(self.FL + (point.z + self.z))
-        return (x_projected+screenX/2, y_projected+screenY/2)
+            y_projected = (self.FL * (point.y - self.y))/(self.FL + (point.z + self.z))
+        return (screenX/2+x_projected, screenY/2-y_projected, can_draw)
 
 class point:
     def __init__(self, x, y, z):
@@ -46,6 +49,9 @@ class point:
             case "y": self.y += amount
             case "z": self.z += amount
 
+    def info(self):
+        print(f"{self.x} {self.y} {self.z}")
+
 # Graphics Functions
 def draw_point(x, y):
     pygame.draw.circle(screen, (0,0,0), (x, y), 5)
@@ -53,7 +59,10 @@ def draw_point(x, y):
 def draw_line(pa, pb):
     pa_projected = game_camera.get_projected(pa)
     pb_projected = game_camera.get_projected(pb)
-    pygame.draw.line(screen, (0,0,0), pa_projected, pb_projected, 2)
+    if not pa_projected[2] and not pb_projected[2]:
+        print("can't draw")
+    else:
+        pygame.draw.line(screen, (0,0,0), (pa_projected[0], pa_projected[1]), (pb_projected[0], pb_projected[1]), 2)
 
 def connect_points(poly_list):
     for poly in poly_list:
@@ -70,12 +79,13 @@ def points_from_model(file_name):
     json_data = json.load(json_file)
     json_index = 1
     mega_point_list = []
-    for poly in json_data: # a poly is just a list of x,y,z tuples
+    for poly in json_data: # a poly is just a list of x,y,z lists
         poly_point_list = []
         for pt in json_data[str(json_index)]: # pt is just a cordinate in 3d space
             poly_point_list.append(point(pt[0], pt[1], pt[2]))
         json_index += 1
         mega_point_list.append(poly_point_list)
+    json_file.close()
     return mega_point_list
 
 def move_points(direction, amount, poly_list):
@@ -83,19 +93,25 @@ def move_points(direction, amount, poly_list):
         for point in poly:
             point.move(direction, amount)
 
-game_camera = camera(200)
+game_camera = camera(500)
 w_held, s_held, a_held, d_held = False, False, False, False
-model_list = ["models//xyz_marker.json", "models//box.json", "models//arcade_machine.json"]
-model_index = 0
-point_list = points_from_model(model_list[model_index])
+model_list = ["models//xyz_marker.json", "models//box.json", "models//arcade_machine.json", "models//room.json"]
+model_index = 1
+poly_list = points_from_model(model_list[model_index])
+movement_speed = 5
+is_jumping = False
+jump_time = 0
 
 running = True
 while running:
     mouseX, mouseY = pygame.mouse.get_rel()
     screen.fill(bg_color)
-    connect_points(point_list)
+    connect_points(poly_list)
     os.system('clear')
     print(mouseX, mouseY)
+    for poly in poly_list:
+        for point_obj in poly:
+            point_obj.info()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -107,20 +123,34 @@ while running:
                 case pygame.K_TAB: 
                     if model_index is not len(model_list)-1: model_index += 1
                     else: model_index = 0
-                    point_list = points_from_model(model_list[model_index])
+                    poly_list = points_from_model(model_list[model_index])
+                case pygame.K_SPACE:
+                    is_jumping = True
                 case pygame.K_w: w_held = True 
                 case pygame.K_s: s_held = True 
                 case pygame.K_a: a_held = True 
                 case pygame.K_d: d_held = True 
+                case pygame.K_ESCAPE: running = False
         if event.type == pygame.KEYUP:
             match event.key:
+                case pygame.K_SPACE:
+                    is_jumping = False
                 case pygame.K_w: w_held = False
                 case pygame.K_s: s_held = False 
                 case pygame.K_a: a_held = False
                 case pygame.K_d: d_held = False
-    if w_held: move_points("z", -10, point_list)
-    if s_held: move_points("z", 10, point_list)
-    if a_held: move_points("x", 10, point_list)
-    if d_held: move_points("x", -10, point_list)
+    if w_held: move_points("z", -movement_speed, poly_list)
+    if s_held: move_points("z", movement_speed, poly_list)
+    if a_held: move_points("x", movement_speed, poly_list)
+    if d_held: move_points("x", -movement_speed, poly_list)
+    if is_jumping or jump_time != 0:
+        if jump_time <= 20:
+            game_camera.y = (math.pow((jump_time-10), 2)*(-1/4)+25)
+            jump_time += 1
+        else:
+            is_jumping = False
+            jump_time = 0
+            game_camera.y = 0
     pygame.display.update()
     clock.tick(FPS)
+            
