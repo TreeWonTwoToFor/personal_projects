7e756027922c77b840fcbebe1b104be5f41d0545
Jonathan Shuman <jonathan@shuman.org>
2024-08-20 09:30:40 -0700

    made a camera system for my second version of the renderer

--- Python/Needs_Work/TreeOS/World/world_package/projection_matrices.py
+++ Python/Needs_Work/TreeOS/World/world_package/projection_matrices.py
@@ -8,24 +8,19 @@ import pygame.draw
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 clock = pygame.time.Clock()
+font = pygame.font.SysFont("Comfortaa", 32)
 
 angle = 0
-
-projection_matrix = [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0]
-]
-
+camera_pos = [0, 0, 400]
 point_matrix = [
-    [1, 1, 1],
-    [1, 1, -1],
-    [1, -1, -1],
-    [1, -1, 1],
-    [-1, -1, 1],
-    [-1, -1, -1],
-    [-1, 1, -1],
-    [-1, 1, 1]
+    [10, 10, 10],
+    [10, 10, -10],
+    [10, -10, -10],
+    [10, -10, 10],
+    [-10, -10, 10],
+    [-10, -10, -10],
+    [-10, 10, -10],
+    [-10, 10, 10]
 ]
 
 def matrix_m(a, b):
@@ -65,40 +60,81 @@ def draw_cube(projected_point_list):
             draw_segment(projected_point_list[i], projected_point_list[0])
             draw_segment(projected_point_list[i+4], projected_point_list[4])
 
+def camera_transform(matrix, camera_position):
+    out_matrix = []
+    for point in matrix: out_matrix.append([0, 0, point[2]])
+    for p in range(len(matrix)):
+        for i in range(len(point)-1):
+            out_matrix[p][i] = matrix[p][i]-camera_position[i]
+    return out_matrix
+
+holding_w, holding_a, holding_s, holding_d = False, False, False, False
+holding_left, holding_right, holding_up, holding_down = False, False, False, False
+angle_x, angle_y, angle_z = 0, 0, 0
 running = True
 while running:
     screen.fill((0,0,0))
-    angle += 0.005
+    text = font.render(f"{camera_pos[0]}, {camera_pos[1]}, {camera_pos[2]}", True, (255, 0, 0))
+    screen.blit(text, (0,0))
     rotation_x = [
         [1, 0, 0],
-        [0, cos(angle), -sin(angle)],
-        [0, sin(angle), cos(angle)]
+        [0, cos(angle_y), -sin(angle_y)],
+        [0, sin(angle_y), cos(angle_y)]
     ]
 
     rotation_y = [
-        [cos(angle), 0, sin(angle)],
+        [cos(angle_x), 0, sin(angle_x)],
         [0, 1, 0],
-        [-sin(angle), 0, cos(angle)]
+        [-sin(angle_x), 0, cos(angle_x)]
     ]
 
     rotation_z = [
-        [cos(angle), -sin(angle), 0],
-        [sin(angle), cos(angle), 0],
+        [cos(angle_z), -sin(angle_z), 0],
+        [sin(angle_z), cos(angle_z), 0],
         [0, 0, 1]
     ]
-    my_matrix = matrix_m(rotation_z, matrix_m(rotation_y, matrix_m(rotation_x, point_matrix)))
+    #my_matrix = matrix_m(rotation_z, matrix_m(rotation_y, matrix_m(rotation_x, camera_transform(point_matrix, camera_pos))))
+    my_matrix = camera_transform(matrix_m(rotation_z, matrix_m(rotation_y, matrix_m(rotation_x, point_matrix))), camera_pos)
+    print(point_matrix)
+    print(camera_pos)
     os.system('cls')
     projected_point_list = []
-    for point in my_matrix: print(point)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    #for point in my_matrix: print(point)
     for point in my_matrix:
         for i in range(len(point)):
-            point[i] = (point[i] * 100)
-        projected_point = (project(point[0], point[2], 1000)+400, project(point[1], point[2], 1000)+400)
+            point[i] = (point[i] * 10)
+        projected_point = (project(point[0], point[2], camera_pos[2])+400, project(point[1], point[2], camera_pos[2])+400)
         projected_point_list.append(projected_point)
         pygame.draw.circle(screen, (0, 200, 50), projected_point, 5)
     draw_cube(projected_point_list)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w: holding_w = True
+            if event.key == pygame.K_a: holding_a = True
+            if event.key == pygame.K_s: holding_s = True
+            if event.key == pygame.K_d: holding_d = True
+            if event.key == pygame.K_LEFT: holding_left = True
+            if event.key == pygame.K_RIGHT: holding_right = True
+            if event.key == pygame.K_UP: holding_up = True
+            if event.key == pygame.K_DOWN: holding_down = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w: holding_w = False
+            if event.key == pygame.K_a: holding_a = False
+            if event.key == pygame.K_s: holding_s = False
+            if event.key == pygame.K_d: holding_d = False
+            if event.key == pygame.K_LEFT: holding_left = False
+            if event.key == pygame.K_RIGHT: holding_right = False
+            if event.key == pygame.K_UP: holding_up = False
+            if event.key == pygame.K_DOWN: holding_down = False
+    if holding_w: camera_pos[2] -= 1
+    if holding_a: camera_pos[0] -= 1
+    if holding_s: camera_pos[2] += 1
+    if holding_d: camera_pos[0] += 1
+    if holding_left: angle_x -= 0.02
+    if holding_right: angle_x += 0.02
+    if holding_up: angle_y -= 0.02
+    if holding_down: angle_y += 0.02
     pygame.display.update()
     clock.tick(60)
