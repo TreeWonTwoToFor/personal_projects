8f8a6802454fcd7538046d0449a85765cbb8a8c1
TreeWonTwoToFor <jonathan-github@shuman.org>
2025-12-09 21:16:39 -0800

    made a working version of flappy bird in my engine!

---
+++ Python/Needs_Work/TreeOS/v4/Engine/FlappyBird.py
@@ -0,0 +1,48 @@
+import pygame
+import math
+
+from Engine import Point
+
+pressing_space = False
+flapped = False
+
+def player_movement(event):
+    global pressing_space
+    if event.type == pygame.KEYDOWN:
+        match event.key:
+            case pygame.K_SPACE: pressing_space = True
+            case pygame.K_ESCAPE: return True
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_SPACE:
+            pressing_space = False
+    return False
+
+def player_movement_update(object_list):
+    global flapped
+    if pressing_space and not flapped:
+        flapped = True
+        object_list[1].translate((0,2,0))
+    elif not pressing_space and flapped:
+        flapped = False
+
+    if object_collision(object_list):
+        print('you died')
+
+def object_collision(object_list):
+    player = object_list[1].collision_values
+    intersect_list = []
+    for obj in object_list[2:]:
+        object_values = obj.collision_values
+        intersect_list.append(
+            player[0] <= object_values[3] and # x low
+            player[1] <= object_values[4] and # y low
+            player[2] <= object_values[5] and # z low
+            player[3] >= object_values[0] and # x max
+            player[4] >= object_values[1] and # y max
+            player[5] >= object_values[2]     # z max
+        )
+    # returns TRUE if intersecting with another object
+    for intersect in intersect_list:
+        if intersect: return True
+    return False
+    
